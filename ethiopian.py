ec8664a90a6a876e158cdd37b85362bd5ac6469f
yewonbahn <banyaewon98@gmail.com>
2023-01-12 09:29:55 +0900

    feat: Add ethiopian multipy
    solve #1 issue

--- ethiopian.py
+++ ethiopian.py
@@ -1,7 +1,16 @@
 # 굿모닝~~ 
 
-def hello():
-    print("Good morning")
+def ehiopian():
+    answer=0 
+    a,b=map(int,input("숫자 두개를 띄어서 입력하세요:").split())
+    while(a>0):
+        if a%2!=0:
+            answer+=b
+        a=a//2
+        b=b*2
+    print(answer)
+    return answer
+
 
 if __name__ == "__main__":
-    hello()
+    ehiopian()
